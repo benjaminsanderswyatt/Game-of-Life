0c66fc996427ab99d1ac8631b0b424e6784b36f9
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2024-09-16 18:52:02 +0100

    Added variable sized grid

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import pygame
 
+RES = WIDTH, HEIGHT = 800, 600
+TILE = 50
+W, H = WIDTH // TILE, HEIGHT // TILE
+FPS = 10
+
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode(RES)
 clock = pygame.time.Clock()
 running = True
-
-dt = 0
-
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 while running:
     # poll for events
@@ -17,25 +18,21 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill(pygame.Color('grey77'))
 
     # RENDER YOUR GAME HERE
-    pygame.draw.circle(screen, "red", player_pos, 40)
+    for x in range(0, WIDTH, TILE):
+        pygame.draw.line(screen, pygame.Color('black'), (x, 0), (x, HEIGHT))
+    for y in range(0, HEIGHT, TILE):
+        pygame.draw.line(screen, pygame.Color('black'), (0, y), (WIDTH, y))
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+
+
 
 
     # flip() the display to put your work on screen
     pygame.display.flip()
 
-    dt = clock.tick(60) / 1000
+    clock.tick(FPS)
 
 pygame.quit()
